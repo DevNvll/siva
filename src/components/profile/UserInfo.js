54db8ebb268bb847b9242d2db4d096d968bd3b20
Henrick Mello <hmartins505@gmail.com>
2020-03-12 04:10:52 -0300

    fixed profile picture sizing

--- src/components/profile/UserInfo.js
+++ src/components/profile/UserInfo.js
@@ -6,7 +6,7 @@ export default function UserInfo({ profile, hasStories }) {
       <img
         src={profile.profile_pic_url}
         className={classNames(
-          "rounded-full shadow-lg self-center md:mr-12 h-40 w-40 sm:m-12",
+          "rounded-full shadow-lg self-center md:mr-12  sm:m-12",
           { "border-pink-800 border-solid border-4": hasStories }
         )}
       />
