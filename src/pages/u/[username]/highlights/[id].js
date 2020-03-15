4836c56e30a1bd40db8cef9726ece9051187a468
Henrick Mello <hmartins505@gmail.com>
2020-03-15 20:18:20 -0300

    Added navbar to stories highlights page

--- src/pages/u/[username]/highlights/[id].js
+++ src/pages/u/[username]/highlights/[id].js
@@ -2,6 +2,8 @@ import getHighlights from "../../../../instagram/getHighlights"
 import StoriesList from "../../../../components/profile/StoriesList"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
+import Head from "next/head"
+import Navbar from "../../../../components/Navbar"
 
 export default function Highlights({ stories }) {
   const router = useRouter()
@@ -10,23 +12,30 @@ export default function Highlights({ stories }) {
   }, [])
 
   return (
-    <div className="p-4 h-screen">
-      <style>{`body {
+    <>
+      <Head>
+        <title>{router.query.username} - Siva</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar search={router.query.username} />
+      <div className="p-4 h-screen pt-16">
+        <style>{`body {
         background-color: #240c3d;
       }`}</style>
-      <a
-        className="text-white font-bold text-5xl mb-10"
-        href={"#"}
-        onClick={() => {
-          router.push("/u/" + router.query.username)
-        }}
-      >
-        {"<"} Back{" "}
-      </a>
-      <div className="mx-auto">
-        <StoriesList stories={stories} />
+        <a
+          className="text-white font-bold text-5xl mb-10"
+          href={"#"}
+          onClick={() => {
+            router.push("/u/" + router.query.username)
+          }}
+        >
+          {"<"} Back{" "}
+        </a>
+        <div className="mx-auto">
+          <StoriesList stories={stories} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
