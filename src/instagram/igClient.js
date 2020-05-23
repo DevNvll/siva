f49805c099e781b990368cc96c6002243b143b4f
Henrick Mello <hmartins505@gmail.com>
2020-05-23 16:18:02 -0300

    pull session from single place

--- src/instagram/igClient.js
+++ src/instagram/igClient.js
@@ -1,10 +1,11 @@
 import { IgApiClient } from "instagram-private-api"
+import restoreSession from "../utils/restoreSession"
 
 export default async function getIgClient() {
   const ig = new IgApiClient()
   ig.state.generateDevice("delta")
 
-  await ig.state.deserialize(JSON.parse(process.env.SESSION_STRING))
+  await ig.state.deserialize(restoreSession())
 
   return ig
 }
