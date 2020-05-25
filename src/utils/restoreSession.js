a99e2e53d69ed4380552bbcc14efdb54fdfd7ac3
Henrick Mello <hmartins505@gmail.com>
2020-05-25 20:31:11 -0300

    Fix restore session

--- src/utils/restoreSession.js
+++ src/utils/restoreSession.js
@@ -1,7 +1,7 @@
-import { readFileSync } from "fs"
-
 export default function restoreSession() {
-  const file = readFileSync("./session/session.json", "utf-8")
-
-  return JSON.parse(process.env.SESSION_STRING || file)
+  try {
+    return JSON.stringify(require("../../session/session.json"))
+  } catch (err) {
+    throw new Error("No session file provided")
+  }
 }
